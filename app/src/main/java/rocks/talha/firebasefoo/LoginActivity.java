1d64cd4bf93d6af37e615f1bb08627b8f8127ca7
mdtalhachy <talhachowdhury3.1416@gmail.com>
2020-09-09 07:19:02 +0600

    Register, Login, Logout Added

---
+++ app/src/main/java/rocks/talha/firebasefoo/LoginActivity.java
@@ -0,0 +1,55 @@
+package rocks.talha.firebasefoo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private Button login;
+
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        login = findViewById( R.id.login);
+
+        auth = FirebaseAuth.getInstance();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email_text = email.getText().toString();
+                String pass_text = password.getText().toString();
+                loginUser(email_text, pass_text);
+            }
+        });
+    }
+
+    private void loginUser(String email_text, String pass_text) {
+        auth.signInWithEmailAndPassword(email_text, pass_text).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/rocks/talha/firebasefoo/StartActivity.java
@@ -0,0 +1,41 @@
+package rocks.talha.firebasefoo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    private Button register;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        register = findViewById(R.id.registerButton);
+        login = findViewById(R.id.loginButton);
+
+        //sending user to RegisterActivity
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, RegisterActivity.class));
+                finish();
+            }
+        });
+
+        //sending users to LoginActivity
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+    }
+}
