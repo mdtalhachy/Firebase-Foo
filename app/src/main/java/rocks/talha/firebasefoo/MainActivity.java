f308c74970850d448e21555f0d31e8e55acbd8b8
mdtalhachy <talhachowdhury3.1416@gmail.com>
2020-09-10 03:59:35 +0600

    Displaying items from editText to listView

--- app/src/main/java/rocks/talha/firebasefoo/MainActivity.java
+++ app/src/main/java/rocks/talha/firebasefoo/MainActivity.java
@@ -1,18 +1,34 @@
 package rocks.talha.firebasefoo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button logout;
+    private EditText name;
+    private Button add;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         logout = findViewById(R.id.logout);
+        name = findViewById(R.id.name);
+        add = findViewById(R.id.add);
+        listView = findViewById(R.id.listView);
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +49,59 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name_text = name.getText().toString();
+                name.setText("");
+                if(name_text.isEmpty()){
+                    Toast.makeText(MainActivity.this, "No Name Entered!", Toast.LENGTH_SHORT).show();
+                }else{
+                    FirebaseDatabase.getInstance().getReference().child("Stack").child("Name").setValue(name_text);
+                }
+            }
+        });
+
+
+        final ArrayList<String> list = new ArrayList<>();
+        Context context;
+        final ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.list_item, list);
+        listView.setAdapter(adapter);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Stack");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list.clear();
+
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    list.add(snapshot.getValue().toString());
+                }
+
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        /*
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("n1", "Java");
+        map.put("n2", "Scala");
+        map.put("n3", "Rust");
+        map.put("n4", "Go");
+        FirebaseDatabase.getInstance().getReference().child("Stack").updateChildren(map);
+         */
+
+        /*
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("Name", "Emtici");
+        map.put("Email", "mtc134@gmail.com");
+        FirebaseDatabase.getInstance().getReference().child("UserDatabase").updateChildren(map);
+        */
     }
 }
